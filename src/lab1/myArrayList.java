a4ea3e1bd17bd8795a28805dd54eb891b32eabc3
EthanGilles <ehgilles01@gmail.com>
2023-09-13 14:49:36 -0400

    myArrayList

--- src/lab1/myArrayList.java
+++ src/lab1/myArrayList.java
@@ -1,10 +1,20 @@
 package lab1;
 
 public class myArrayList {
-	public static final int INITIAL_CAPACITY = 50;
+	private static final int INITIAL_CAPACITY = 50;
+	private static final double GROWTH_RATE= 0.2;
+	private Fraction[] list = null;
+	private int size = INITIAL_CAPACITY;
 	
 	
 	
+	public myArrayList() {
+		list = new Fraction[size];
+    }
+
+	public myArrayList(int n) {
+		list = new Fraction[n];
+	}
 	
 	public boolean add(Fraction input ) {
 		
@@ -20,5 +30,7 @@ public class myArrayList {
 		
 		
 	}
+	
+	
 	
 }
