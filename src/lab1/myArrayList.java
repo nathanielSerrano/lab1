e81d81649c018686a1c41ff373f73ef4d72cd1f5
EthanGilles <ehgilles01@gmail.com>
2023-09-13 14:35:37 -0400

    myArrayList

---
+++ src/lab1/myArrayList.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class myArrayList {
+	public static final int INITIAL_CAPACITY = 50;
+	
+	
+	
+	
+	public boolean add(Fraction input ) {
+		
+		return false;
+	}
+	
+	public Fraction remove(Fraction input) {
+		
+		
+	}
+	
+	public boolean contains(Fraction input) {
+		
+		
+	}
+	
+}
