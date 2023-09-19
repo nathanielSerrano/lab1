330f40fff21d9f04d4c6e3229305cdbdcaee33c8
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2023-09-19 18:51:53 -0400

    Update myArrayList.java

--- src/lab1/myArrayList.java
+++ src/lab1/myArrayList.java
@@ -4,7 +4,8 @@ public class myArrayList {
 	private static final int INITIAL_CAPACITY = 50;
 	private static final double GROWTH_RATE= 0.2;
 	private Fraction[] list = null;
-	private int size = INITIAL_CAPACITY;
+	private int capacity = INITIAL_CAPACITY;
+	private int size = 0;
 	
 	
 	
@@ -18,14 +19,22 @@ public class myArrayList {
 	
 	
 	  public boolean add(Fraction input) {
-        if (list.size() >= capacity) {
-            //Calculate new growth rate is the list is full
+	        
+		  /** this can be done with my reconfigure method
+		  if (list.length() >= capacity) {
+            // If the list is full, calculate the new capacity based on the growth policy
             int newCapacity = (int) (capacity + capacity * growthPolicy);
             list.ensureCapacity(newCapacity);
             capacity = newCapacity;
         }
+        **/
+		 
+		if(size >= capacity)
+			reconfigure();
+		
         
-        return list.add(input);
+       // return list.add(input);
+		return false;
     }
 
 	
@@ -65,7 +74,34 @@ public class myArrayList {
         }
         return false; // Fraction not found in the list
     }
+	public int Size() {
+		return size;
+		
+	}
 	
 	
+	/**Ethan Gilles
+	 * whenever the array is full, call reconfigure. This will copy the current array into a bigger one
+	 * and set the internal array to it
+	 */
+	public void reconfigure() {
+		
+		//changes the capacity of the array
+		capacity += capacity*GROWTH_RATE;
+		
+		
+		//new fraction array of correct size
+		Fraction[] bigArray = new Fraction[capacity];
+		
+		//set values equal
+		for(int i = 0; i < size; i++) {
+			bigArray[i] = list[i];
+		}
+		
+		//array that is instanced with the arrayList is turned into the big array.
+		list = bigArray;
+	}
 	
 }
+
+
