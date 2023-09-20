34b49dc87ecfc8ffea1b5a25c5f9fa5067b060fc
nathanielSerrano <144270767+nathanielSerrano@users.noreply.github.com>
2023-09-20 00:04:06 -0400

    Update myArrayList.java

--- src/lab1/myArrayList.java
+++ src/lab1/myArrayList.java
@@ -10,7 +10,7 @@ public class myArrayList {
 	
 	
 	public myArrayList() {
-		list = new Fraction[size];
+		list = new Fraction[INITIAL_CAPACITY];
     }
 
 	public myArrayList(int n) {
@@ -21,23 +21,36 @@ public class myArrayList {
 		 myArrayList myList = new myArrayList();
 
 	     // Test the add method
-	     myList.add(new Fraction(1, 2));
-	     myList.add(new Fraction(3, 4));
+		 Fraction frac1 = new Fraction(1, 2);
+		 Fraction frac2 = new Fraction(3, 4);
+		 Fraction frac3 = new Fraction(5, 6);
+	     myList.add(frac1);
+	     myList.add(frac2);
+	     myList.add(frac3);
+	     System.out.println("Original list: ");
 	     for (int i=0;i<myList.size(); i++) {
-	    	 myList.get(i).print();
+	    	 System.out.print(myList.get(i).toString()+", ");
 	     }
+	     System.out.println();
 
 	     // Test the remove method
 	     Fraction removedFraction = myList.remove(new Fraction(1, 2));
 	     if (removedFraction != null) {
-	         System.out.println("Removed Fraction: "); removedFraction.print();
+	         System.out.println("Removed Fraction: "+ removedFraction.toString());
 	     } else {
 	         System.out.println("Fraction not found.");
 	     }
+	     System.out.println("Modified list: ");
+	     for (int i=0;i<myList.size(); i++) {
+    	 System.out.print(myList.get(i).toString() + ", ");
+     }
 
 	     // Test the contains method
 	     boolean containsFraction = myList.contains(new Fraction(3, 4));
-	     System.out.println("Contains Fraction (3/4): " + containsFraction);
+	     System.out.println("\nContains Fraction (3/4): " + containsFraction);
+	     containsFraction = myList.contains(new Fraction(1,2));
+	     System.out.println("Contains Fraction (1/2): "+ containsFraction);
+	     
 	 }
 
 	//Coby Andersen
@@ -54,40 +67,42 @@ public class myArrayList {
 		  size++;
 		  return true;
     }
- /**
-  * Nathaniel Serrano
-  * remove() - Takes a Fraction object as input and returns input if the 
-  * object is successfully removed from the list; Otherwise returns null
-  * @param input - Fraction object
-  * @return - Returns input if object is successfully removed from list;
-  * otherwise returns null if input is not found in list.
-  */
+
+	/**
+	 * Nathaniel Serrano
+	 * remove() - Takes a Fraction object as input and returns input if the 
+	 * object is successfully removed from the list; Otherwise returns null
+	 * @param input - Fraction object
+	 * @return - Returns input if object is successfully removed from list;
+	 * otherwise returns null if input is not found in list.
+	 */
 	public Fraction remove(Fraction input) {
 		int indexOfInput=0;
-	     // Check if the item to remove exists in the array
-	    boolean found = false;
-	        for (int i = 0; i < list.length; i++) {
-	            if (list[i].hasSameValue(input)) {
-	                found = true;
-	                indexOfInput=i;
-	                break;
-	            }
-	        }
-	    if (!found) {
-	        return null;
-	        }
-	    // Create a new array to hold the result with one less element
-	    int numMoved = size - indexOfInput - 1;
-	    if (numMoved > 0)
-	    System.arraycopy(list, indexOfInput+1, list, indexOfInput, numMoved);
-	    list[--size] = null;
-	        
-	    return input;
-	}
+		      // Check if the item to remove exists in the array
+        boolean found = false;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].hasSameValue(input)) {
+                found = true;
+                indexOfInput=i;
+                break;
+            }
+        }
+        if (!found) {
+            return null;
+        }
+        // Create a new array to hold the result with one less element
+        int numMoved = size - indexOfInput - 1;
+        if (numMoved > 0)
+        System.arraycopy(list, indexOfInput+1, list, indexOfInput, numMoved);
+        list[--size] = null;
+        
+        return input;
+    }
 		
 	
 	
-	/**
+/**
+ * Mandy Ho
  * Checks if the given Fraction object exists in the array.
  *
  * @param input The Fraction object to check for existence.
@@ -95,11 +110,12 @@ public class myArrayList {
  */
 public boolean contains(Fraction input) {
     for (int i = 0; i < list.length; i++) {
-        if (list[i].hasSameValue(input)) {
+    	if (list[i]==null)
+    		return false;
+        if (list[i].hasSameValue(input))
             return true; // Found a matching fraction
-        }
     }
-    return false; // Fraction not found in the list
+    return false; // Fraction not found in the list 
 }
 
 	
